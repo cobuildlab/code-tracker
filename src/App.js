1cd521e20aa87b860b6b6a9bc206f0ffc73906ca
Angel Lacret <alacret@gmail.com>
2019-10-07 21:28:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,28 @@ import PrivateRoute from "./components/PrivateRoute";
 // New - import the React Router components, and the Profile page component
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Profile from "./components/Profile";
+import createAuth0Client from "@auth0/auth0-spa-js";
+import {clientId, domain} from './auth_config';
+
 
 function App() {
+
+  const connect = async () => {
+    const auth0 = await createAuth0Client({
+      domain,
+      client_id: clientId
+    });
+
+    const user = await auth0.getUser();
+    //logged in. you can get the user profile like this:
+    // const user = await auth0.getUser();
+    console.log(user.sub);
+
+    // const {token} = JSON.parse(localStorage.getItem('auth'));
+    // console.log(token);
+    fetch(`http://localhost:5000/auth?token=${user.sub}`).then(res => res.json()).then(console.log)
+  };
+
   return (
     <div className="App">
       {/* New - use BrowserRouter to provide access to /profile */}
@@ -15,7 +35,19 @@ function App() {
           <NavBar/>
         </header>
         <Switch>
-          <Route path="/" exact/>
+          <Route path="/" exact component={() => {
+            return (
+              <div>
+                {/*<NavBar/>*/}
+                <div>HELLO HOME!</div>
+                <div>
+                  <button onClick={connect}>
+                    CONNECT
+                  </button>
+                </div>
+              </div>
+            )
+          }}/>
           <PrivateRoute path="/profile" component={Profile}/>
         </Switch>
       </BrowserRouter>
